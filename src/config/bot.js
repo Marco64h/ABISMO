7dd8e951ea9282b52e5c2b377b0006719c8402ca
Marco64h <42558109+Marco64h@users.noreply.github.com>
2026-09-24 12:22:21 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "você", // required by Discord API, not shown in the client
-        state: "a",     // this is what people actually see
+        state: "3",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
