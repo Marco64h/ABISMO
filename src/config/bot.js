4f282d322b3b38ae102274a1eb8b5f5ef965159a
Marco64h <42558109+Marco64h@users.noreply.github.com>
2026-09-24 12:06:59 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "V̶̨͙͎̗͎̏͋͆͊̆̕o̶̡̱̺͇̬͕̘̭͕̽͌͐̏̔̾͋̑̊̓̅́̌͠͠c̸̢̲̘̲̣̙̟̰͎̯̔̍̀̓͋̋̀̀͝ê̵̼̥̱̖̮̻̫͖̱̂̾̌̏͐͂̄͘",     // this is what people actually see
+        state: "a",     // this is what people actually see
         type: 3,               // Custom
       },
     ],
